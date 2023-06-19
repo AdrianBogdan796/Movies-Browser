146fc696e34c7772778d0aaa4273870ae2653891
Adrian Bogdan <enu999@o2.pl>
2023-06-19 23:24:52 +0200

    Movie tile

--- src/Movie list/Movie tile/index.js
+++ src/Movie list/Movie tile/index.js
@@ -1,29 +1,19 @@
-import {
-  Tile,
-  TileTitle as Title,
-  TileSubtitle as Subtitle,
-  TileTag as Tag,
-  TileTags as Tags,
-} from "../../components/Tile";
-import { MoviesPage } from "../features/movies/MoviesPage";
-import poster from "./images/poster.png";
+import * as styled from "./styled";
+import poster from "../../images/poster.png";
 
-<Title> Popular movies </Title>;
-function MoviesPage() {
-  return (
-    <>
-      <Tile>
-        <Image src={poster} alt="" />
-        <Title>Mulan</Title>
-        <Subtitle>2020</Subtitle>
-        <Tags>
-          <Tag>Action</Tag>
-          <Tag>Action</Tag>
-          <Tag>Action</Tag>
-        </Tags>
-      </Tile>
-    </>
-  );
-}
+<styled.Header>Popular movies</styled.Header>;
+
+return MoviesPage(
+  <styled.Tile>
+    <styled.TileImage src={poster} alt="" />
+    <styled.TileTitle>Mulan</styled.TileTitle>
+    <styled.TileSubtitle>2020</styled.TileSubtitle>
+    <styled.TileTags>
+      <styled.TileTag>Action</styled.TileTag>
+      <styled.TileTag>Action</styled.TileTag>
+      <styled.TileTag>Action</styled.TileTag>
+    </styled.TileTags>
+  </styled.Tile>
+);
 
 export default MoviesPage;
